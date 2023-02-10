45cbe99b38eacd7c0e37ca2d4a156de444aff934
uo263630 <uo263630@uniovi.es>
2023-02-10 12:48:15 +0100

    new file:   sesion3/index.html 	modified:   sesion3/prac3-2.html 	modified:   sesion3/src/prac3-2.js 	modified:   sesion3/src/prac3-4.js 	modified:   sesion3/style.css

--- sesion3/src/prac3-2.js
+++ sesion3/src/prac3-2.js
@@ -12,73 +12,73 @@ if ( WEBGL.isWebGLAvailable() ) {
     const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
     camera.position.set( 0, 0, 300 );
 
-    
-    const video = document.getElementById( 'video' );
 
-    
     const startButton = document.getElementById('startButton');
     startButton.addEventListener('click', () => init(), false);
 
     function init() {
         var overlay = document.getElementById('overlay');
         overlay.remove();
+        
 
-        // Do stuff
         const video = document.getElementById('video');
+        const image = document.createElement( 'canvas' );
+        image.width = 480;  // Video width
+        image.height = 204; // Video height
+        const imageContext = image.getContext( '2d' );
+        imageContext.fillStyle = '#000000';
+        imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
+        const texture = new THREE.Texture( image );
+        
+        const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+        const material = new THREE.MeshBasicMaterial(
+        {
+            map: texture
+        } );
+        
+        const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
+
+        
+
+        scene.add( wall );
+        renderer.render( scene, camera);
+
+        window.addEventListener( 'resize', ( ) => {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix( );
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            renderer.render( scene, camera );
+        }, false );
+
+
+        
+        const clock = new THREE.Clock( );
+
+        function animate( ) {
+
+            const delta = clock.getDelta( ); // Elapsed time in seconds
+
+            // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
+            const rotation = ( delta * Math.PI * 2 ) / 5;
+            
+            wall.rotation.y += rotation;
+
+            if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+
+                imageContext.drawImage( video, 0, 0 );
+                if ( texture ) texture.needsUpdate = true;
+            }
+            // Request the browser to execute the animation-rendering loop
+            requestAnimationFrame( animate );
+            // Render the scene
+            renderer.render( scene, camera );
+
+            
+        };
+        
+        animate();
         video.play();
     }
-
-    const image = document.createElement('canvas');
-    image.width = 480;  // Video width
-    image.height = 204; // Video height
-    const imageContext = image.getContext('2d');
-    imageContext.fillStyle = '#000000';
-    imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
-    const texture = new THREE.Texture( image );
-
-    
-    const material = new THREE.MeshBasicMaterial( { map: texture } );
-    const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
-
-    if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
-
-        imageContext.drawImage( video, 0, 0 );
-        if ( texture ) texture.needsUpdate = true;
-    }
-
-
-    scene.add( wall );
-    renderer.render( scene, camera );
-
-    window.addEventListener( 'resize', ( ) => {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix( );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.render( scene, camera );
-    }, false );
-
-
-    
-    const clock = new THREE.Clock( );
-
-    function animate( ) {
-        
-        const delta = clock.getDelta( ); // Elapsed time in seconds
-
-        // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
-        const rotation = ( delta * Math.PI * 2 ) / 24;
-        
-        wall.rotation.y += rotation;
-
-        // Render the scene
-        renderer.render( scene, camera );
-
-
-    };
-    
-    animate();
-
-
 }
 
 
--- sesion3/src/prac3-4.js
+++ sesion3/src/prac3-4.js
@@ -1,7 +1,6 @@
 import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
 import * as THREE from 'three';
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min';
-import Stats from 'three/examples/jsm/libs/stats.module';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 
 if ( WEBGL.isWebGLAvailable() ) {
     // WebGL is available
@@ -13,18 +12,24 @@ if ( WEBGL.isWebGLAvailable() ) {
     document.body.appendChild( renderer.domElement );
 
     const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
-    camera.position.set( 0, 20, 300 );
+    camera.position.set( 0, 20, 400 );
 
 
-    const specialFaceMaterial = Material// Material for a face
-    const regularFaceMaterial = "../textures/brick.jpg"// Material for the rest of the faces
+   
     const geometry = new THREE.BoxGeometry( 50, 50, 50 );
-    const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
-    const material = new THREE.MeshPhongMaterial(
+    const textureLoader = new THREE.TextureLoader( );  // The object used to load textures 
+    const specialFaceMaterial = new THREE.MeshPhongMaterial(
+    {
+        map: textureLoader.load( "../textures/cubo.png" , ( loaded ) => { renderer.render( scene, camera ); }),
+        bumpMap: textureLoader.load( "../textures/cubo-map.png" , ( loaded ) => { renderer.render( scene, camera ); })
+    } );// Material for a face
+    
+    const regularFaceMaterial = new THREE.MeshPhongMaterial(
     {
         map: textureLoader.load( "../textures/brick.jpg" , ( loaded ) => { renderer.render( scene, camera ); }),
         bumpMap: textureLoader.load( "../textures/brick-map.jpg" , ( loaded ) => { renderer.render( scene, camera ); })
-    } );
+    } );// Material for the rest of the faces
+
     const materials = [
         specialFaceMaterial,
         regularFaceMaterial,
@@ -33,37 +38,68 @@ if ( WEBGL.isWebGLAvailable() ) {
         regularFaceMaterial,
         regularFaceMaterial
     ];
+    
+    materials.bumpScale =4;
     const box = new THREE.Mesh( geometry, materials );
 
-    const light = new THREE.AmbientLight( 0xFFFFFF );
-    const light2 = new THREE.PointLight( 0xffffff, 2, 400 );
-    light2.position.set( -100,0,100 );
+    //const light = new THREE.AmbientLight( 0xFFFFFF );
+    const light2 = new THREE.PointLight( 0xffffff, 4, 150 );
+    light2.position.set( -130,20,-50 );
 
 
-    box.position.set(-100,10,0);
+    box.position.set(-200,25,-100);
 
 
 
     const geometry2 = new THREE.BoxGeometry( 50, 50, 50 );
     const textureLoader2 = new THREE.TextureLoader( );  // The object used to load textures
-    const material2 = new THREE.MeshPhongMaterial(
+    const specialFaceMaterial2 = new THREE.MeshPhongMaterial(
+    {
+        map: textureLoader2.load( "../textures/cubo.png" , ( loaded ) => { renderer.render( scene, camera ); })
+    } );// Material for a face
+        
+    const regularFaceMaterial2 = new THREE.MeshPhongMaterial(
     {
         map: textureLoader2.load( "../textures/brick.jpg" , ( loaded ) => { renderer.render( scene, camera ); })
-    } );
-    const box2 = new THREE.Mesh( geometry2, material2 );
+    } );// Material for the rest of the faces
+
+        const materials2 = [
+            regularFaceMaterial2,
+            specialFaceMaterial2,
+            regularFaceMaterial2,
+            regularFaceMaterial2,
+            regularFaceMaterial2,
+            regularFaceMaterial2
+        ];
+    const box2 = new THREE.Mesh( geometry2, materials2 );
     
-    box2.position.set(100,10,0);
+    box2.position.set(200,25,-100);
 
     const helper = new THREE.GridHelper( 800, 40, 0x444444, 0x444444 );
     helper.position.y = 0.1;
     const light3 = new THREE.DirectionalLight( 0xffffff, 0.5 );
     light3.position.set( 0,0.5,100 );
 
-    scene.add( box , light, box2,helper, light3);
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xf0f0f0, 0.6 );
+    hemiLight.position.set( 0, 500, 0 );
+ 
+
+    const controls = new FirstPersonControls( camera, renderer.domElement );
+    controls.movementSpeed = 70;
+    controls.lookSpeed = 0.05;
+    controls.noFly = false;
+    controls.lookVertical = false;
+
+
+
+    scene.add( box , light2, box2 ,helper, light3, hemiLight, controls);
+
+    
     renderer.render( scene, camera);
 
  
 
+    
 
     window.addEventListener( 'resize', ( ) => {
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -72,20 +108,14 @@ if ( WEBGL.isWebGLAvailable() ) {
         renderer.render( scene, camera );
     }, false );
 
-
-    
     const clock = new THREE.Clock( );
 
     function animate( ) {
 
         const delta = clock.getDelta( ); // Elapsed time in seconds
-
-        // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
-        const rotation = ( delta * Math.PI * 2 ) / 24;
         
-        box.rotation.y += rotation;
-        box2.rotation.y += rotation;
-        
+        controls.update( delta );
+    
         
         // Request the browser to execute the animation-rendering loop
         requestAnimationFrame( animate );
@@ -95,7 +125,8 @@ if ( WEBGL.isWebGLAvailable() ) {
         
     };
     
-    //animate();
+    animate();
+
 
 }
 

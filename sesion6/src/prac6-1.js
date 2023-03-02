6d431c0fad78d1dc288f37d83671264950c353bd
uo263630 <uo263630@uniovi.es>
2023-03-02 12:08:14 +0100

    Sesion6 	new file:   sesion6/.gitignore 	new file:   sesion6/estilo.css 	new file:   sesion6/package-lock.json 	new file:   sesion6/package.json 	new file:   sesion6/report.css 	new file:   sesion6/style.css 	new file:   sesion6/webpack.config.js

---
+++ sesion6/src/prac6-1.js
@@ -0,0 +1,110 @@
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
+import * as THREE from 'three';
+import adapter from 'webrtc-adapter';
+
+if ( WEBGL.isWebGLAvailable() ) {
+    // WebGL is available
+    const scene = new THREE.Scene();
+
+    const renderer = new THREE.WebGLRenderer( {antialias: true} );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+    camera.position.set( 0, 0, 300 );
+
+/*
+    //const startButton = document.getElementById('startButton');
+    //startButton.addEventListener('click', () => init(), false);
+
+    function init() {
+        var overlay = document.getElementById('overlay');
+        overlay.remove();    
+
+        const video = document.getElementById('player');
+        const image = document.createElement( 'canvas' );
+        image.width = 480;  // Video width
+        image.height = 204; // Video height
+        const imageContext = image.getContext( '2d' );
+        imageContext.fillStyle = '#000000';
+        imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
+        const texture = new THREE.Texture( image );
+        
+        const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+        const material = new THREE.MeshBasicMaterial(
+        {
+            map: texture
+        } );
+        
+        const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
+
+        const url = "http://localhost:60080/video1/counter.mpd";
+        const player = dashjs.MediaPlayer().create();
+        player.initialize(document.querySelector("#player"), url, true);
+
+        scene.add( wall );
+        renderer.render( scene, camera);
+
+        window.addEventListener( 'resize', ( ) => {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix( );
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            renderer.render( scene, camera );
+        }, false );
+
+
+        video.play();
+    }
+
+*/   
+
+    const video = document.querySelector( 'video' );
+    const constraints = {
+        audio: false,
+        video: true
+    };
+    
+
+    window.addEventListener( 'resize', ( ) => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix( );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.render( scene, camera );
+    }, false );
+
+    navigator.mediaDevices.getUserMedia( constraints )
+        // Called when we get the requested streams
+        .then( ( stream ) => {
+
+            // Video tracks (usually only one)
+            const videoTracks = stream.getVideoTracks( );
+            console.log( 'Stream characteristics: ', constraints );
+            console.log( 'Using device: ' + videoTracks[0].label );
+
+            // End of stream handler
+            stream.onended = () => {
+
+                console.log( 'End of stream' );
+            };
+
+            // Bind the stream to the html video element
+            video.srcObject = stream;
+    })
+        // Called in case of error
+        .catch( ( error ) => {
+
+            if ( error.name === 'ConstraintNotSatisfiedError' ) {
+
+                console.error( 'The resolution ' + constraints.video.width.exact + 'x' +
+                            constraints.video.width.exact + ' px is not supported by the camera.' );
+            } else if ( error.name === 'PermissionDeniedError' ) {
+
+                console.error( 'The user has not allowed the access to the camera and the microphone.' );
+            }
+            console.error( ' Error in getUserMedia: ' + error.name, error );
+    });
+
+
+}
+
+
